cc14e7fc51e9399b0b6e5b79c1f6f37af5dd5691
Henri Drake <henrifdrake@gmail.com>
2020-11-19 10:57:11 -0500

    Updated ocean & climate model structs

--- 4_ocean_heat_transport.jl
+++ 4_ocean_heat_transport.jl
@@ -50,7 +50,7 @@ md"""
 ##### 1.1) The two-dimensional advection-diffusion equation
 Recall from **Lecture 22** that the one-dimensional advection-diffusion equation is written as
 
-$\frac{\partial T(x,t)}{\partial t} = -U \frac{\partial T}{\partial x} + \kappa \frac{\partial^{2} T}{\partial x^{2}},$
+$\frac{\partial T(x,t)}{\partial t} = -U \frac{\partial T}{\partial x} + \kappa \frac{\partial^{2} T}{\partial x^{2}} - (A - BT)/C + S(1-α)/4/C,$
 
 where $T(x, t)$ is the temperature, $U$ is a constant advective velocity and $\kappa$ is the diffusivity.
 
@@ -161,12 +161,6 @@ struct Parameters
 	κ::Float64
 end
 
-# ╔═╡ 59f9da1e-2a10-11eb-14c0-0b292af2d42f
-struct Velocity
-	u::Array{Float64, 2}
-	v::Array{Float64, 2}
-end
-
 # ╔═╡ 9c3643ea-2a09-11eb-2d38-1589b871ae4a
 begin
 	import Base.zeros
@@ -178,26 +172,56 @@ begin
 	struct OceanModel
 		G::Grid
 		P::Parameters
-		
-		Δt::Float64
-		T::Array{Float64, 2}
+
 		u::Array{Float64, 2}
 		v::Array{Float64, 2}
 	end
 
-	OceanModel(G, P, T, Δt) = OceanModel(G, P, Δt, T, zeros(G), zeros(G))
-	OceanModel(G, P, T, Δt) = OceanModel(G, P, Δt, zeros(G), zeros(G), zeros(G))
+	OceanModel(G, P) = OceanModel(G, P, zeros(G), zeros(G))
+	OceanModel(G) = OceanModel(G, Parameters(1.e4), zeros(G), zeros(G))
 end;
 
+# ╔═╡ f92086c4-2a74-11eb-3c72-a1096667183b
+begin
+	struct ClimateModel
+		O::OceanModel
+		
+		T::Array{Float64, 2}
+		Δt::Float64
+	end
+end
+
 # ╔═╡ 3d12c114-2a0a-11eb-131e-d1a39b4f440b
-function InitBox(G; value=1.)
+function InitBox(G; value=1., nx=2, ny=2)
 	T = zeros(G)
-	T[G.Ny÷2-2:G.Ny÷2+2, G.Nx÷2-2:G.Nx÷2+2] .= value
+	T[G.Ny÷2-ny:G.Ny÷2+ny, G.Nx÷2-nx:G.Nx÷2+nx] .= value
 	return T
 end
 
-# ╔═╡ 0586bff6-2a1a-11eb-25f6-d70c9b7c7f79
-1
+# ╔═╡ 2b3d2062-2a73-11eb-368e-f563b2ad7aba
+md"""
+Let's check that the total heat of the ocean is conserved: 
+"""
+
+# ╔═╡ d042d25a-2a62-11eb-33fe-65494bb2fad5
+begin
+	quiverBox = @bind show_quiver CheckBox(default=false)
+	anomalyBox = @bind show_anomaly CheckBox(default=false)
+	md"""
+	*Click to show the velocity field* $(quiverBox) *or to show temperature **anomalies** instead of absolute values* $(anomalyBox)
+	"""
+end
+
+# ╔═╡ 933d42fa-2a67-11eb-07de-61cab7567d7d
+begin
+	κ_ex_Slider = @bind κ_ex Slider(0.:1e5, default=1e4, show_value=true)
+	md"""
+	*Vary the diffusivity κ* $(κ_ex_Slider)
+	"""
+end
+
+# ╔═╡ c9ea0f72-2a67-11eb-20ba-376ca9c8014f
+@bind go_ex Clock(0.3)
 
 # ╔═╡ 9036dc6a-204e-11eb-305d-45e760e62bef
 begin
@@ -216,7 +240,7 @@ begin
 	diffuse(T, κ, Δy, Δx) = [
 		diffuse(T, κ, Δy, Δx, j, i) for j=2:size(T, 1)-1, i=2:size(T, 2)-1
 	]
-	diffuse(O::OceanModel) = diffuse(O.T, O.P.κ, O.G.Δy, O.G.Δx)
+	diffuse(T, O::OceanModel) = diffuse(T, O.P.κ, O.G.Δy, O.G.Δx)
 end
 
 # ╔═╡ 1cea2b90-205d-11eb-0d06-7df64faf1b53
@@ -240,7 +264,7 @@ begin
 		for j=2:size(T, 1)-1, i=2:size(T, 2)-1
 	]
 	
-	advect(O::OceanModel) = advect(O.T, O.u, O.v, O.G.Δy, O.G.Δx)
+	advect(T, O::OceanModel) = advect(T, O.u, O.v, O.G.Δy, O.G.Δx)
 end
 
 # ╔═╡ b68ca886-2053-11eb-2e39-35c724ed3a3c
@@ -255,9 +279,17 @@ end
 # ╔═╡ c4424838-12e2-11eb-25eb-058344b39c8b
 begin
 	# Initial conditions
-	linearT(G) = [ -(y/G.L) for y in G.y[:, 1], x in G.x[1, :] ]
+	linearT(G) = 0.5*(1. .+[ -(y/G.L) for y in G.y[:, 1], x in G.x[1, :] ])
 	t = Ref(0.)
 end;
+
+# ╔═╡ d2c27a5c-2a77-11eb-0772-3b8b0d4cf682
+md"""
+$\mathcal{O}(\text{timestep})*\mathcal{O}(\text{diffusion / advection}) = N * N^2$
+"""
+
+# ╔═╡ a138d27e-2a77-11eb-2d5a-09a717f94e7d
+md"$\Delta t < \frac{\Delta x}{max(v)}$"
 
 # ╔═╡ f5ae1756-12e9-11eb-1228-8f03879c154a
 md"""
@@ -270,10 +302,35 @@ md"""
 """
 
 # ╔═╡ 87bfc240-12e3-11eb-03cc-756dc00efa6c
-function timestep!(O)
-	update_ghostcells!(O.T)
-	O.T[2:end-1, 2:end-1] .+= O.Δt*(advect(O) .+ diffuse(O))
+function timestep!(C)
+	update_ghostcells!(C.T)
+	C.T[2:end-1, 2:end-1] .+= C.Δt*(advect(C.T, C.O) .+ diffuse(C.T, C.O))
 end;
+
+# ╔═╡ 16905a6a-2a78-11eb-19ea-81adddc21088
+begin
+	
+	Nvec = 1:25
+	tvec = map(Nvec) do Npower
+		G = Grid(8*Npower, 6.e6);
+		P = Parameters(κ_ex);
+
+		#u, v = DoubleGyre(G)
+		#u, v = PointVortex(G, Ω=0.5)
+		u, v = zeros(G), zeros(G)
+		
+		O = OceanModel(G, P, u, v)
+		
+		IC = InitBox(G)
+		#IC = InitBox(G, nx=G.Nx÷2-1)
+		#IC = linearT(G)
+
+		Δt = 6*60*60
+		C = ClimateModel(O, copy(IC), Δt)
+
+		return @elapsed timestep!(C)
+	end
+end
 
 # ╔═╡ 440fe49a-12e5-11eb-1c08-f706f5f33c84
 @bind go Clock()
@@ -289,20 +346,35 @@ end;
 # end
 
 # ╔═╡ c0e46442-27fb-11eb-2c94-15edbda3f84d
-function plot_state(O)
+function plot_state(C; clims=(-1.1,1.1), show_quiver=true, IC=nothing)
+	O = C.O
 	X = repeat(O.G.x, O.G.Ny, 1)
 	Y = repeat(O.G.y, 1, O.G.Nx)
-	p = contourf(O.G.x[:], O.G.y[:], O.T,
-		color=:bluesreds, levels=-10:1:10,
-		colorbar_title="Temperature [°C]", clims=(-10, 10)
-	)
+	if show_anomaly
+		arrow_col = :black
+		maxdiff = maximum(abs.(O.T .- IC))
+		p = contourf(O.G.x[:], O.G.y[:], C.T .- IC, clims=(-maxdiff, maxdiff),
+			color=:balance, colorbar_title="Temperature [°C]", linewidth=0.,
+			size=(400,530)
+		)
+	else
+		arrow_col = :lightblue
+		p = contourf(O.G.x[:], O.G.y[:], C.T,
+			color=:thermal, levels=clims[1]:(clims[2]-clims[1])/21.:clims[2],
+			colorbar_title="Temperature [°C]", clims=clims,
+			linewidth=0., size=(400,530)
+		)
+	end
 	
-	Nq = O.G.N÷6
-	quiver!(p,
-		X[(Nq+1)÷2:Nq:end], Y[(Nq+1)÷2:Nq:end],
-		quiver=O.G.L*3 .*(O.u[(Nq+1)÷2:Nq:end], O.v[(Nq+1)÷2:Nq:end]),
-		color=:black, alpha=0.7
-	)
+	
+	Nq = O.G.N÷5
+	if show_quiver
+		quiver!(p,
+			X[(Nq+1)÷2:Nq:end], Y[(Nq+1)÷2:Nq:end],
+			quiver=O.G.L*4 .*(O.u[(Nq+1)÷2:Nq:end], O.v[(Nq+1)÷2:Nq:end]),
+			color=arrow_col, alpha=0.7
+		)
+	end
 	plot!(p,
 		yticks=( (-O.G.L:1000e3:O.G.L), 1e-3*(-O.G.L:1000e3:O.G.L) ),
 		xticks=( (0:1000e3:O.G.L), 1e-3*(0:1000e3:O.G.L) ),
@@ -327,6 +399,14 @@ The solution makes use of an advanced *asymptotic method* (valid in the limit th
 
 
 
+# ╔═╡ df706ebc-2a63-11eb-0b09-fd9f151cb5a8
+function impose_no_flux!(u, v)
+	u[1,:] .= 0.; v[1,:] .= 0.;
+	u[end,:] .= 0.; v[end,:] .= 0.;
+	u[:,1] .= 0.; v[:,1] .= 0.;
+	u[:,end] .= 0.; v[:,end] .= 0.;
+end
+
 # ╔═╡ e3ee80c0-12dd-11eb-110a-c336bb978c51
 begin
 	∂x(ϕ, Δx) = (ϕ[:,2:end] - ϕ[:,1:end-1])/Δx
@@ -345,12 +425,32 @@ begin
 	end
 end
 
+# ╔═╡ e2e4cfac-2a63-11eb-1b7f-9d8d5d304b43
+function PointVortex(G; Ω=1., a=0.2, x0=0.5, y0=0.)
+	x = reshape(0. -G.Δx/(G.L):G.Δx/G.L:1. +G.Δx/(G.L), (1, G.Nx+1))
+	y = reshape(-1. -G.Δy/(G.L):G.Δy/G.L:1. +G.Δy/(G.L), (G.Ny+1, 1))
+	
+	function ψ̂(x,y)
+		r = sqrt.((y .-y0).^2 .+ (x .-x0).^2)
+		
+		stream = -Ω/4*r.^2
+		stream[r .> a] = -Ω*a^2/4*(1. .+ 2*log.(r[r .> a]/a))
+		
+		return stream
+	end
+		
+	u, v = diagnose_velocities(ψ̂(x, y), G)
+	impose_no_flux!(u, v)
+	
+	return u,v
+end
+
 # ╔═╡ ecaab27e-2a16-11eb-0e99-87c91e659cf3
 function DoubleGyre(G; β=2e-11, τ₀=0.1, ρ₀=1.e3, ν=1.e5, κ=1.e5, H=1000.)
 	ϵM = ν/(β*G.L^3)
 	ϵ = ϵM^(1/3.)
-	x_ = reshape(0. -G.Δx/(G.L):G.Δx/G.L:1. +G.Δx/(G.L), (1, G.Nx+1))
-	y_ = reshape(-1. -G.Δy/(G.L):G.Δy/G.L:1. +G.Δy/(G.L), (G.Ny+1, 1))
+	x = reshape(0. -G.Δx/(G.L):G.Δx/G.L:1. +G.Δx/(G.L), (1, G.Nx+1))
+	y = reshape(-1. -G.Δy/(G.L):G.Δy/G.L:1. +G.Δy/(G.L), (G.Ny+1, 1))
 	
 	ψ̂(x,y) = π*sin.(π*y) * (
 		1 .- x - exp.(-x/(2*ϵ)) .* (
@@ -359,15 +459,8 @@ function DoubleGyre(G; β=2e-11, τ₀=0.1, ρ₀=1.e3, ν=1.e5, κ=1.e5, H=1000
 			)
 		.+ ϵ*exp.((x .- 1.)/ϵ)
 	)
-	
-	function impose_no_flux!(u, v)
-		u[1,:] .= 0.; v[1,:] .= 0.;
-		u[end,:] .= 0.; v[end,:] .= 0.;
-		u[:,1] .= 0.; v[:,1] .= 0.;
-		u[:,end] .= 0.; v[:,end] .= 0.;
-	end
 		
-	u, v = (τ₀/ρ₀)/(β*G.L*H) .* diagnose_velocities(ψ̂(x_, y_), G)
+	u, v = (τ₀/ρ₀)/(β*G.L*H) .* diagnose_velocities(ψ̂(x, y), G)
 	impose_no_flux!(u, v)
 	
 	return u, v
@@ -375,31 +468,46 @@ end
 
 # ╔═╡ 863a6330-2a08-11eb-3992-c3db439fb624
 begin
-	G = Grid(30, 6.e6);
-	P = Parameters(1.e5);
+	G = Grid(20, 6.e6);
+	P = Parameters(κ_ex);
+	
 	u, v = DoubleGyre(G)
-	O = OceanModel(G, P, 6*60*60, InitBox(G), u, v)
+	#u, v = PointVortex(G, Ω=0.5)
+	#u, v = zeros(G), zeros(G)
+
+	IC = InitBox(G)
+	#IC = InitBox(G, nx=G.Nx÷2-1)
+	#IC = linearT(G)
+	
+	O = OceanModel(G, P, u, v)
+	Δt = 6*60*60
+	
+	C = ClimateModel(O, copy(IC), Δt)
 end;
-
-# ╔═╡ b1446ac2-2a16-11eb-2ae9-49267b95a185
-sum(O.T)
-
-# ╔═╡ 83c5dbb2-2a0a-11eb-0b1d-d120efa14de5
-begin
-	timestep!(O)
-	#surface(O.G.x[:], O.G.y[:], O.T)
-	#plot(O.T[:,O.G.Nx÷2], ylims=(0, 1))
-end
 
 # ╔═╡ 3b4e4722-12fe-11eb-238d-17aea2c23f58
 begin
-	CFL_adv(O) = maximum(O.v)*O.Δt/O.G.Δx
-	CFL_diff(O) = O.P.κ*O.Δt/(O.G.Δx^2)
-	CFL_adv(O), CFL_diff(O)
+	CFL_adv(C::ClimateModel) = maximum(C.O.v)*C.Δt/C.O.G.Δx
+	CFL_diff(C::ClimateModel) = C.O.P.κ*C.Δt/(C.O.G.Δx^2)
+	CFL_adv(C), CFL_diff(C)
 end
 
 # ╔═╡ d96c7a56-12e4-11eb-123c-d57487bd37df
 as_svg(x) = PlutoUI.Show(MIME"image/svg+xml"(), repr(MIME"image/svg+xml"(), x))
+
+# ╔═╡ 83c5dbb2-2a0a-11eb-0b1d-d120efa14de5
+begin
+ 	go_ex	
+	for i in 1:150
+		timestep!(C)
+	end
+	plot_state(C, clims=(0., 1), show_quiver=show_quiver, IC=IC)
+end |> as_svg
+
+# ╔═╡ 794c2148-2a78-11eb-2756-5bd28b7726fa
+begin
+	plot(8*Nvec, tvec, xlabel="Number of Grid Cells (in x-direction)", ylabel="elapsed time per timestep [s]")
+end |> as_svg
 
 # ╔═╡ 6b3b6030-2066-11eb-3343-e19284638efb
 plot_kernel(A) = heatmap(
@@ -426,14 +534,16 @@ plot_kernel(adv_kernel[:, 0:0])
 # ╠═b1b5625e-211a-11eb-3ee1-3ba9c9cc375a
 # ╠═cd2ee4ca-2a06-11eb-0e61-e9a2ecf72bd6
 # ╠═2a93145e-2a09-11eb-323b-01817062aa89
-# ╠═59f9da1e-2a10-11eb-14c0-0b292af2d42f
 # ╠═9c3643ea-2a09-11eb-2d38-1589b871ae4a
 # ╠═d3796644-2a05-11eb-11b8-87b6e8c311f9
+# ╠═f92086c4-2a74-11eb-3c72-a1096667183b
 # ╠═863a6330-2a08-11eb-3992-c3db439fb624
 # ╟─3d12c114-2a0a-11eb-131e-d1a39b4f440b
-# ╠═b1446ac2-2a16-11eb-2ae9-49267b95a185
+# ╟─2b3d2062-2a73-11eb-368e-f563b2ad7aba
+# ╟─d042d25a-2a62-11eb-33fe-65494bb2fad5
+# ╠═933d42fa-2a67-11eb-07de-61cab7567d7d
+# ╟─c9ea0f72-2a67-11eb-20ba-376ca9c8014f
 # ╠═83c5dbb2-2a0a-11eb-0b1d-d120efa14de5
-# ╠═0586bff6-2a1a-11eb-25f6-d70c9b7c7f79
 # ╠═9036dc6a-204e-11eb-305d-45e760e62bef
 # ╟─fd07ee24-2067-11eb-0ac8-7b3da3993223
 # ╠═79a0086c-2050-11eb-1974-49d430b5eecd
@@ -442,6 +552,10 @@ plot_kernel(adv_kernel[:, 0:0])
 # ╠═16b72cfc-2114-11eb-257d-b7747a99e155
 # ╠═b68ca886-2053-11eb-2e39-35c724ed3a3c
 # ╠═c4424838-12e2-11eb-25eb-058344b39c8b
+# ╠═16905a6a-2a78-11eb-19ea-81adddc21088
+# ╠═794c2148-2a78-11eb-2756-5bd28b7726fa
+# ╠═d2c27a5c-2a77-11eb-0772-3b8b0d4cf682
+# ╠═a138d27e-2a77-11eb-2d5a-09a717f94e7d
 # ╠═3b4e4722-12fe-11eb-238d-17aea2c23f58
 # ╟─f5ae1756-12e9-11eb-1228-8f03879c154a
 # ╟─f9824610-12e7-11eb-3e61-f96c900a0636
@@ -450,6 +564,8 @@ plot_kernel(adv_kernel[:, 0:0])
 # ╠═bd879bbe-12de-11eb-0d1d-93bba42b6ff9
 # ╠═c0e46442-27fb-11eb-2c94-15edbda3f84d
 # ╟─bb084ace-12e2-11eb-2dfc-111e90eabfdd
+# ╠═df706ebc-2a63-11eb-0b09-fd9f151cb5a8
+# ╠═e2e4cfac-2a63-11eb-1b7f-9d8d5d304b43
 # ╠═ecaab27e-2a16-11eb-0e99-87c91e659cf3
 # ╠═e3ee80c0-12dd-11eb-110a-c336bb978c51
 # ╠═9c8a7e5a-12dd-11eb-1b99-cd1d52aefa1d
